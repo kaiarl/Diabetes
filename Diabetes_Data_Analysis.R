d3657b3bf4a583c694a4929e8cbf479312ac8a84
kaia-lindberg <kaia.lindberg@gmail.com>
2021-11-15 20:25:32 -0500

    Add visualizations for proposal

--- Diabetes_Data_Analysis.R
+++ Diabetes_Data_Analysis.R
@@ -28,3 +28,51 @@ print(summary(Data))
 # Check for missing values in each column
 colSums(is.na(Data))
 
+# Create diabetes factor column with labels from outcome column 
+Data$Diabetes<-factor(Data$Outcome)
+levels(Data$Diabetes) <- c("No", "Yes")
+
+# Visualizations for project proposal
+# For logistic regression
+# Distribution of response variable
+ggplot(Data, aes(x=Diabetes)) +
+  geom_bar(fill="blue") +
+  labs(x="Diabetes Outcome", y="Frequency", title="Distribution of Diabetes Outcome")
+
+# Scatter plot of potential predictors (colored by response)
+# bmi and blood pressure colored by diabetes
+ggplot(Data, aes(x=BMI, y=BloodPressure, color=Diabetes)) +
+  geom_point() + 
+  labs(x="BMI", y="Blood Pressure", title="Scatterplot of BMI versus Blood Pressure by Diabetes")
+
+# Side by side box plot for potential predictor by response
+ggplot(Data, aes(x=Diabetes, y=Age))+
+  geom_boxplot(color = "blue", outlier.color = "orange") +
+  labs(title="Distribution of Age by Diabetes Outcome")
+
+ggplot(Data, aes(x=Diabetes, y=Glucose))+
+  geom_boxplot(color = "blue", outlier.color = "orange") +
+  labs(title="Distribution of Glucose by Diabetes Outcome")
+
+
+# For linear regression
+# Scatter plot of potential regressor versus response (glucose)
+# BMI
+ggplot(Data, aes(x=BMI, y=Glucose)) +
+  geom_point() + 
+  geom_smooth(method="lm", se=F) +
+  labs(x="BMI", y="Glucose", title="Scatterplot of BMI versus Glucose")
+
+# Blood pressure
+ggplot(Data, aes(x=BloodPressure, y=Glucose)) +
+  geom_point() + 
+  geom_smooth(method="lm", se=F) +
+  labs(x="Blood Pressure", y="Glucose", title="Scatterplot of Blood Pressure versus Glucose")
+
+
+# Insulin
+ggplot(Data, aes(x=Insulin, y=Glucose)) +
+  geom_point() + 
+  geom_smooth(method="lm", se=F) +
+  labs(x="Insulin", y="Glucose", title="Scatterplot of Insulin versus Glucose")
+
